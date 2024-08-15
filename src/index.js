0d1ce010c85b4692fceced2a2a8b539e1cbe139a
lighting129 <devrajajoo@gmail.com>
2024-08-15 22:31:04 +0530

    ccc

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 import {
   BrowserRouter as Router,
   Route,
   Switch,
   Redirect,
-} from 'react-router-dom'
+} from "react-router-dom";
 
-import './style.css'
-import Home from './views/home'
-import Login from './views/login'
-import SignUp from './views/sign-up'
-import NotFound from './views/not-found'
+import "./style.css";
+import Home from "./views/home";
+import Login from "./views/login";
+import SignUp from "./views/sign-up";
+import NotFound from "./views/not-found";
+import Dashboard from "./views/Dashboard";
+import Report from "./views/Report";
 
 const App = () => {
   return (
@@ -21,10 +23,12 @@ const App = () => {
         <Route component={Login} exact path="/login" />
         <Route component={SignUp} exact path="/sign-up" />
         <Route component={NotFound} path="**" />
+        <Route component={Report} path="/Report" />
+        <Route component={Dashboard} path="/Dashboard" />
         <Redirect to="**" />
       </Switch>
     </Router>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById("app"));
